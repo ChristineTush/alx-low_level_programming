3ac33033f9b6faeb5bf4d4ca91a8456ef4afbb78
ChristineTush <christine@gmail.com>
2022-03-23 19:16:09 +0000

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,20 +1,25 @@
 #include "main.h"
 
 /**
- * _strncpy - Entry point
- * @dest: copy to
- * @src: copy from
- * @n: input number of char
- * Return: Always 0 (Success)
+ * _strncpy - two words
+ * @dest : pointer to char params
+ * @src : pointer to char params
+ * @n : int params
+ * Return: *dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
-	for (i = 0; src[i] != '\0' && i < n; i++)
-		if (i < n)
-			dest[i] = src[i];
-	while (i < n)
-		dest[i++] = '\0';
-	
-	return (dest);
+int i;
+
+for (i = 0; src[i] != '\0' && i < n; i++)
+{
+dest[i] = src[i];
+}
+
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
 }
