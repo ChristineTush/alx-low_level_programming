04719157eafa641a467fa2e7a0103a979257e492
ChristineTush <christine@gmail.com>
2022-03-31 03:20:37 +0000

    pow recursion

--- 0x08-recursion/4-pow-recursion.c
+++ 0x08-recursion/4-pow-recursion.c
@@ -1,23 +1,17 @@
 #include "main.h"
 
 /**
- * _pow_recursion - Search a string for any of a set of bytes.
- * @x: base
- * @y: exposant
- * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
- * or NULL if no such byte is found.
+ * _pow_recursion - Entry point
+ * @x: input one
+ * @y: input two
+ * Return: Always 0 (Success)
  */
-
 int _pow_recursion(int x, int y)
 {
-
-if (y < 0)
-	return (-1);
-else if (y == 1)
-	return (x);
-else if (y == 0)
-	return (1);
-
-return (x * _pow_recursion(x, y - 1));
-
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
 }
