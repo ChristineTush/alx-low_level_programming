3e72ca794224acac946e51b2a49cf23cc3321c22
ChristineTush <christine@gmail.com>
2022-04-04 20:00:49 +0000

    static lib

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -10,11 +10,11 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-
+	
 	for (i = 0; src[i] != '\0'; i++)
 		if (i < n)
 			dest[i] = src[i];
-		while (i < n)
+	while (i < n)
 			dest[i++] = '\0';
 
 	return (dest);
