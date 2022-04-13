831f82f6354b42308fa14c03527a73c0b5d22428
ChristineTush <christine@gmail.com>
2022-04-13 21:40:59 +0000

    function that executes a function given as a parameter on each element of an array

---
+++ 0x0F-function_pointers/1-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_elem - prints an integer
+ * @elem: the integer to print
+ *
+ * Return: Nothing.
+ */
+void print_elem(int elem)
+{
+    printf("%d\n", elem);
+}
+
+/**
+ * print_elem_hex - prints an integer, in hexadecimal
+ * @elem: the integer to print
+ *
+ * Return: Nothing.
+ */
+void print_elem_hex(int elem)
+{
+    printf("0x%x\n", elem);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int array[7] = {1, 2, 3, 4, 5, 6, 7};
+
+    array_iterator(array, 7, &print_elem);
+    array_iterator(array, 7, &print_elem_hex);
+    return (0);
+}
