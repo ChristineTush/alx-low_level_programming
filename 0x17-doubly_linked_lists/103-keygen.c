35341caef19afb48f092a91476d2465e0725de73
ChristineTush <christine@gmail.com>
2022-06-09 23:39:55 +0000

    trial

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,52 +1,42 @@
-#include "stdafx.h"
-#include <Windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-/*
- * Author: @OsandaMalith
- * Website: https://osandamalith.com
+/**
+ * main - random password generator for 101-crackme
+ *
+ * Return: always 0
  */
+int main(void)
+{
+	int i, j, k, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
 
-void check(int *pid) {
-	TCHAR computerName[MAX_COMPUTERNAME_LENGTH + 1];
-	DWORD size = sizeof(computerName) / sizeof(computerName[0]);
-	GetComputerName(computerName, &size);
-	printf("[+] Computer Name: %s\n", computerName);
-
-	TCHAR computerUserName[MAX_COMPUTERNAME_LENGTH + 1];
-	size = sizeof computerUserName / sizeof *computerUserName;
-	GetUserName(computerUserName, &size);
-	printf("[+] Username: %s\n\n", computerUserName);
-
-	int name = strlen((char *)(&computerName));
-	int username = strlen((char *)(&computerUserName));
-
-	int charName = *(char *)((int)&computerName);
-	int charUser = *(char *)((int)&computerUserName);
-
-	for (int i = 1; i < name; i++)
-		charName += *(char *)(i + (int)&computerName);
-
-	for (int i = 1; i < username; i++)
-		charUser += *(char *)(i + (int)&computerUserName);
-	
-	int a = 9999 ^ *pid;
-	int b = 2222 ^ charUser;
-	int c = (a - b) ^ charName;
-
-	printf("[~] Serial Key: %d-%d-%d\n\n",c,2222,9999);
-	puts("[*] If the above doesn't work try this:\n");
-	a = 9999 ^ *pid;
-	b = 2222 ^ charUser;
-	c = (a - b) ^ 0;
-
-	printf("[~] Serial Key: %d-%d-%d\n\n", c, 2222, 9999);
-}
-
-int _tmain(int argc, _TCHAR* argv[]){
-	puts("\t\t\t[~] caneacsu's CrackMe Keygen\n\t\t\t[~] Coded by @OsandaMalith");
-	int pid = 0;
-	printf("\n\n[?] Enter PID: ");
-	scanf("%d", &pid);
-	check(&pid);
-	return 0;
+	srand(time(NULL));
+	while (s != 2772)
+	{
+		i = k = s = 0;
+		while ((2772 - 122) > s)
+		{
+			j = rand() % 62;
+			p[i] = c[j];
+			s += c[j];
+			i++;
+		}
+		while (c[k])
+		{
+			if (c[k] == (2772 - s))
+			{
+				p[i] = c[k];
+				s += c[k];
+				i++;
+				break;
+			}
+			k++;
+		}
+	}
+	p[i] = '\0';
+	printf("%s", p);
+	return (0);
 }
