761c03c6d5cf821903be795a4e862701a716c01c
ChristineTush <christine@gmail.com>
2022-06-13 22:02:49 +0000

    dynamic lib

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
- wget -q -O /tmp/librandy1.so https://github.com/kabingu-sam/BatchScripts/raw/master/librandy1.so
- export LD_PRELOAD=/tmp/librandy1.so
+wget -P /tmp https://github.com/edwinogwel/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so
